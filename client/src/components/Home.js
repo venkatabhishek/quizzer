3913d4948746002329c0d601ad9b47b692376894
root <root@DESKTOP-VFULB7D.localdomain>
2019-05-17 21:28:12 -0400

    home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import logo from '../logo.svg';
+import Link from '@material-ui/core/Link';
 
 const styles = theme => ({
-	card: {
-		maxWidth: 700,
-		margin: 'auto',
-		marginTop: theme.spacing.unit * 5
+	title : {
+		"color": "rgba(0, 0, 0, 0.9)"
+	}, 
+	main: {
+		position: "fixed",
+  		top: "45%",
+  		left: "50%",
+  		transform: "translate(-50%, -50%)",
+		textAlign: "center",
+
 	},
-	title: {
-		padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme
-			.spacing.unit * 2}px`,
-		color: theme.palette.text.secondary,
-		fontSize: 24
+	links: {
+		color: "rgba(0, 0, 0, 0.9)",
+		float: "left"
 	},
-	media: {
-		minHeight: 450
+	linksContainer: {
+		textAlign: "center",
+		width: "100%"
+	},
+	link:{
+		margin: 10
 	}
 });
 
@@ -27,23 +32,20 @@ class Home extends Component {
 	render() {
 		const { classes } = this.props;
 		return (
-			<div>
-				<Card className={classes.card}>
-					<Typography type="headline" component="h2" className={classes.title}>
-						Welcome to the MERN APP
+			<div className={classes.main}>
+				<Typography variant="h3" gutterBottom className={classes.title}>
+        			Quizzer
+      			</Typography>
+				  <div className={classes.linksContainer}>
+					<Typography variant="h5" gutterBottom className={classes.links}>
+					<Link className={classes.link} color="inherit">
+						Signup
+					</Link>
+					<Link className={classes.link} color="inherit" href={"/signin"}>
+						Login
+					</Link>
 					</Typography>
-					<CardMedia
-						className={classes.media}
-						image={logo}
-						title="Auth with MERN"
-					/>
-					<CardContent>
-						<Typography type="body1" component="p">
-							This is a demo application that uses a Node + MongoDB API for user
-							authentication. Built With React + Material UI.
-						</Typography>
-					</CardContent>
-				</Card>
+				</div>
 			</div>
 		);
 	}
