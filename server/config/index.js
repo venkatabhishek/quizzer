cc73955c8844253f7dda5eafa415cb3cd9cff16b
Abhishek Venkat <abhisaivenkat@gmail.com>
2019-06-22 17:16:03 +0000

    hide

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 const config = {
 	port: process.env.PORT || 3000,
 	jwtSecret: process.env.JWT_SECRET || 'mkT23j#u!45',
-	mongoURI: process.env.MONGODB_URI || 'mongodb+srv://abhi:as70rv65@cluster0-efqto.mongodb.net/test?retryWrites=true'
+	mongoURI: process.env.MONGODB_URI || '****'
 };
 
 export default config;
