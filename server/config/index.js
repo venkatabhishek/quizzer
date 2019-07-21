72d3eb184be73e73fb2371aa335ed520c9c02892
Abhishek Venkat <abhisaivenkat@gmail.com>
2019-07-21 11:36:48 -0400

    fix port

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-	port: process.env.PORT || 4000,
+	port: process.env.PORT || 3000,
 	jwtSecret: process.env.JWT_SECRET || 'mkT23j#u!45',
 	mongoURI: process.env.MONGODB_URI || 'mongodb+srv://abhi:as70rv65@cluster0-efqto.mongodb.net/test?retryWrites=true&w=majority'
 };
